3b6ef1d57061867b3bb9641d30031cbae52ff6be
Hritwik Tripathi <Tripathi.hritwik@gmail.com>
2020-07-14 20:50:40 +0530

    new files added

--- kewl/src/App.js
+++ kewl/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import { Navbar ,NavbarBrand } from 'reactstrap';
 import './App.css';
+import Menu from './components/MenuComponent';
 
 function App() {
   return (
@@ -11,21 +12,6 @@ function App() {
             <NavbarBrand href ="/">Restraunt New Home</NavbarBrand>
         </div>
       </Navbar>
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
 
     </div>
   );
